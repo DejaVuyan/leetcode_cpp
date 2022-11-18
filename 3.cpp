e1afb79bb9ea9d3f51ed25c2533d87f2b3153f81
46752 <yangyuzun@163.com>
2022-11-18 18:15:06 +0800

    最终提交版本

--- 3.cpp
+++ 3.cpp
@@ -11,15 +11,16 @@ using namespace std;
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int max_l = 1;
+        int max_l = 0;
+        int tmp_l = 0;  //记录每一次循环不重复的值
         unordered_set<char> vet;
         // 字符串长度为0或者1的情况?
         // 字符串为空
         if (s.size()==0) return 0;
         if (s.size()==1) return 1;
 
-        for(int p_l=0,p_r=1;p_l<s.size();p_l++,p_r=p_l+1){
-            // 开始索引更新后，删除哈希表中开始元素前面的元素
+        for(int p_l=0,p_r=1;p_l<s.size();p_l++,p_r=max(p_l+tmp_l-1,p_l+1)){
+            // 开始索引更新后，删除哈希表中开始元素前面的元素,应该清楚所有元素或者索引从
             if (p_l!=0) vet.erase(s[p_l-1]);   
             // 插入左指针的值
             vet.insert(s[p_l]);  // 插入哈希表
@@ -28,9 +29,9 @@ public:
                 //p_r保持上一次的最大无重复子串索引，这样写有个问题就是p_r不会更新，可能出现p_l>p_r的情况
                 p_r++;   
             }
-            max_l=max(max_l,(p_r-p_l));
+            tmp_l = p_r-p_l;
+            max_l=max(max_l,tmp_l);
             // 等于0的话是不用删除的
-            
         }
         return max_l;
     }
@@ -38,7 +39,9 @@ public:
 
 int main(){
     // 定义测试用例，或者是用键盘输入的方式定义
-    string s="pwwkew";
+    //测试用例
+    // "aab" 2; "pwwkew" 3;"anviaj" 5 
+    string s="uqinntq";
     // 初始化类
     Solution sol;
     int ret = sol.lengthOfLongestSubstring(s);
