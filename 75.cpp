754cfa667334f7ff09e5f5623295cdce52bdea43
46752 <yangyuzun@163.com>
2022-12-07 10:30:27 +0800

    75题单指针循环两次的结果

--- 75.cpp
+++ 75.cpp
@@ -8,18 +8,18 @@ using namespace std;
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int p0=0,p1=0;
+        int p=0;
+        // 第一遍遍历，把0都放到数组前面
         for (int i=0;i<nums.size();i++){
-            if (nums[i]==1){
-                swap(nums[p1],nums[i]);
-                p1++;
+            if (nums[i]==0){
+                swap(nums[p],nums[i]);
+                p++;
             }
-            else if (nums[i]==0)
-            {
-                swap(nums[p0],nums[i]);
-                if (p0<p1) swap(nums[p1],nums[i]);
-                p0++;
-                p1++;
+        }
+        for (int i=p;i<nums.size();i++){
+            if (nums[i]==1){
+                swap(nums[p],nums[i]);
+                p++;
             }
         }
     }
